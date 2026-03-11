84b1b71ae3dce1b778038b1040495328a8191db6
ArtemKudriavskyi <artem.kudriavskyi@gmail.com>
2026-03-11 12:47:36 +0200

    update project

--- src/App.js
+++ src/App.js
@@ -1,33 +1,61 @@
 import { useEffect, useState } from "react";
-import axios from 'axios'
+import axios from "axios";
+
 function App() {
   const [data, setData] = useState({});
-  const [town, setTown] = useState("cherkasy");
+  const [town, setTown] = useState("");
 
   const key = "fe294bed1253726bee131e464889685d";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${town}&units=metric&appid=${key}&lang=ua`;
+  const defaultTown = "cherkasy";
+  const [classes, setClasses] = useState("");
 
-  // const [kiev,setKiev]=useState()
-  useEffect(()=>{
-    axios.get(url).then(response=>{
-      setData(response.data);
-      console.log(response.data)
-  })
-  },[])
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${defaultTown}&units=metric&appid=${key}&lang=ua`,
+      )
+      .then((response) => {
+        setData(response.data);
+        // console.log(response.data)
+        setBackground(response.data)
+      });
+  }, []);
+
   function searchWeather(event) {
     if (event.key === "Enter") {
-      axios.get(url).then(response=>setData(response.data ))
-      // fetch(url)
-      //   .then((response) => response.json())
-      //   .then((response) => {
-      //     setData(response);
-      //     console.log(response);
-      //   });
+      axios.get(url).then((response) => {
+        setData(response.data);
+        setBackground(response.data);
+        console.log(response.data);
+        console.log(classes);
+      });
       setTown("");
     }
   }
+
+  function setBackground(data) {
+    console.log(1);
+    // console.log(data.weather[0].main)
+    let newClass='default'
+    switch (data.weather[0].main) {
+      case "Clouds":
+        newClass="clouds";
+        break;
+      case "Clear":
+        
+        newClass="clear"
+        break;
+      case "Rain":
+       newClass="rain"
+        break;
+      default:
+        newClass="default"
+    }
+    setClasses(newClass+' app')
+  }
   return (
-    <div className="app">
+    <div className={classes}>
       <div className="input-field">
         <input
           type="text"
